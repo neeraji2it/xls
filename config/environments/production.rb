d7bf2041baaba3946b60b3d45bc202b82508ef1d
Rails <railsG>
2016-10-08 13:19:12 +0530

    egfyg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,18 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host:'xls12.herokuapp.com' }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.default_url_options = { host: 'xls12.herokuapp.com' }
+config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => 'xls12.herokuapp.com',
-      :user_name => "onlinesticker12151@gmail.com",
-      :password => "Nagaraju@2015",
-      :authentication => :plain,
-      :enable_starttls_auto => true
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    #:domain               => 'baci.lindsaar.net',
+    :user_name            => 'onlinesticker12151@gmail.com',
+    :password             => 'Nagaraju@2015', #'dinemedia!@#',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
   }
 end
