7abea05327e7323bc08336140d310133880e43a1
Rails <railsG>
2016-10-14 14:58:46 +0530

    ada gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :trackable, :validatable
-protected
-
-def confirmation_required?
-    self.user.nil?
-end
 end
