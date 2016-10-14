9d880bd03a9fd0027717f7d4dc729347d79b5ff0
Rails <railsG>
2016-10-14 15:59:30 +0530

    ada gemfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,15 +76,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-   config.action_mailer.default_url_options = { host: 'xls12.herokuapp.com' }
-config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { :host => 'xls12.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    #:domain               => 'baci.lindsaar.net',
-    :user_name            => 'kammaranagaraju@gmail.com',
-    :password             => 'Nagaraju@2015', #'dinemedia!@#',
-    :authentication       => 'plain',
-    :enable_starttls_auto => true
+    address: "smtp.sendgrid.net",
+    port: 25,
+    domain: "xls12.herokuapp.com",
+    authentication: "plain",
+    user_name: ENV["kammaranagaraju@gmail.com"],
+    password: ENV["Nagaraju@2015"]
   }
 end
